5eb3e94e6df25c83deaaf300811d3003912a673f
Parshantrao <parshantrao2017@gmail.com>
2022-10-18 13:07:25 +0530

    cartApi's

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -32,7 +32,7 @@ const createCart = async function(req,res){
             return res.status(400).send({status:false, message:"Invalid productId"})
         }
 
-        const product = await productModel.findOne({_id:productId,isDeleted:false}).lean()
+        const product = await productModel.findOne({_id:productId,isDeleted:false}).lean().select({title:1,price:1,productImage:1})
         if(!product){
             return res.status(400).send({status:false, message:"No product found"})
         }
@@ -44,40 +44,46 @@ const createCart = async function(req,res){
             if(!validator.isValidObjectId(cartId)){
                 return res.status(400).send({status:false, message:"Invalid cartId"})
             }
+            if(cartId!=cart._id){
+                return res.status(400).send({status:false, message:"different cartId is present"})
+            }
 
             let {items,totalItems,totalPrice}=cart
             let flag = true
+
             for(let i=0;i<items.length;i++){
                 if(items[i].productId==productId){
                     flag=false
-                    items[i].quantity=items[i].quantity++
+                    items[i].quantity++
                 }
             }
             if(flag){
                 let obj={
-                    productId:mongoose.Types.ObjectId(productId),
+                    productId:productId,
                     quantity:1
                 }
                 items.push(obj)
             }
-            totalPrice=totalPrice+product.price
+            totalPrice+=product.price
             totalItems=items.length
+            const updatedCart = await cartModel.findByIdAndUpdate(cartId,{$set:{items,totalPrice,totalItems}},{new:true}).populate("items.productId","title price productImage")
             
-            const updatedCart = await cartModel.findByIdAndUpdate(cartId,{items,totalPrice,totalItems},{new:true})
             return res.status(201).send({status:true, message:"Success", data:updatedCart})
         }
         else{
             let obj={}
             obj.userId = mongoose.Types.ObjectId(userId)
+
             obj.items= [{
-                productId:mongoose.Types.ObjectId(productId),
+                productId:productId,
                 quantity:1
             }]
             obj.totalItems=1
             obj.totalPrice=product.price
-
             const newCart = await cartModel.create(obj)
-            return res.status(201).send({status:true, message:"Success", data:newCart})
+            let newObj = JSON.parse(JSON.stringify(newCart))
+            newObj.items[0].productId=product
+            return res.status(201).send({status:true, message:"Success", data:newObj})
         }
     }
     catch(err){
@@ -127,40 +133,42 @@ const updateCart = async function(req,res){
         }
 
         if(removeProduct==0){
-            const items = cart.items
-            const flag = true
+            let {items,totalItems,totalPrice} = cart
+            let flag = true
             for(let i=0;i<items.length;i++){
-                
-                if(items[i].productId==mongoose.Types.ObjectId(productId)){
+                if(items[i].productId==(productId)){
                     flag =false
+                    totalPrice -= product.price*items[i].quantity
                     items.splice(i,i+1)
                 }
             }
+            totalItems = items.length
             if(flag){
                 return res.status(400).send({status:false, message:"this product was not added in cart"})
             }
-            const updatedCart = await cartModel.findByIdAndUpdate(cartId,{$set:{items}},{new:true})
-            return res.status(200).send({status:true, data:product})
+            const updatedCart = await cartModel.findByIdAndUpdate(cartId,{$set:{items,totalItems,totalPrice}},{new:true}).populate("items.productId","title price productImage")
+            return res.status(200).send({status:true, data:updatedCart})
         }
 
         else if (removeProduct==1){
-            const items = cart.items
-            const flag = true
+            let {items,totalItems,totalPrice} = cart
+            let flag = true
             for(let i=0;i<items.length;i++){
-                
-                if(items[i].productId==mongoose.Types.ObjectId(productId)){
+                if(items[i].productId==productId){
                     flag =false
                     items[i].quantity=items[i].quantity-1
+                    totalPrice -= product.price
                     if(items[i].quantity==0){
                         items.splice(i,i+1)
                     }
                 }
             }
+            totalItems = items.length
             if(flag){
                 return res.status(400).send({status:false, message:"this product was not added in cart"})
             }
-            const updatedCart = await cartModel.findByIdAndUpdate(cartId,{$set:{items}},{new:true})
-            return res.status(200).send({status:true, data:product})
+            const updatedCart = await cartModel.findByIdAndUpdate(cartId,{$set:{items,totalItems,totalPrice}},{new:true}).populate("items.productId","title price productImage")
+            return res.status(200).send({status:true, data:updatedCart})
         }
     }
     catch(err){
@@ -184,20 +192,12 @@ const getCart = async function(req,res){
             return res.status(404).send({status:false, message:"no user found"})
         }
 
-        const cart = await cartModel.findOne({userId}).lean()
+        const cart = await cartModel.findOne({userId}).populate("items.productId","title price productImage")
         if(!cart){
             return res.status(404).send({status:false, message:"no cart found"})
         }
 
-        let productId = []
-        for(let i=0;i<cart.items.length;i++){
-            let ID = cart.items[i].productId
-            productId.push(ID)
-        }
-
-        const product = await productModel.find({_id:{$in:[...productId]}})
-
-        return res.status(200).send({status:true, cart:cart, data:product})
+        return res.status(200).send({status:true, message:"Success", data:cart})
     }
     catch(err){
         return res.status(500).send({status:false, message:err.message})
@@ -219,19 +219,18 @@ const deleteCart = async function(req,res){
         if(!user){
             return res.status(400).send({status:false, message:"No user found"}) 
         }
-
         const obj={
-            itmes:[],
+            items:[],
             totalItems:0,
             totalPrice:0
         }
-
-        const cart = await cartModel.findOne({userId},{$set:obj},{new:true})
+        const cart = await cartModel.findOneAndUpdate({userId},{$set:obj},{new:true}) 
+       
         if(!cart){
             return res.status(404).send({status:false, message:"no cart found"})
         }
-        console.log(cart)
-        return res.status(204).send({status:true, message:"cart deleted"})
+        
+        return res.status(204).send()
 
     }
     catch(err){
